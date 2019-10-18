4b10376238f394f733c07ca14da65db5ef4331ab
Arkadiusz Wronski <arkadiusz.wronski@pl.pwc.com>
2019-10-18 13:22:49 +0200

    Init commit

---
+++ src/main/java/com/pl/sample/pipesfilters/PipesFiltersApplication.java
@@ -0,0 +1,13 @@
+package com.pl.sample.pipesfilters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipesFiltersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipesFiltersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pl/sample/pipesfilters/PipesFiltersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pl.sample.pipesfilters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipesFiltersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
